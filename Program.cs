d44e7cc444d7e63ba258f262abdeb7d291bfc4b8
Dmitry Kh <dnhaziev@mail.ru>
2024-02-29 16:05:01 +0500

    Add program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Создание массива
+string[] textList = {"Hello", "224", "world", ":-)", "try", "ui"};
+
+//Подсчет количества элементов с необходимой длинной
+int CountSymbol (string[] textList) {
+    int count = 0;
+    for (int i=0; i < textList.Length; i++){
+        if (textList[i].Length <= 3) {
+            count ++;
+        }
+    }
+    return count;
+}
+
+//Создание массива необходимой длины
+string[] textListLen3 = new string[CountSymbol(textList)];
+
+//ЗАполнение массива элементами
+void AddStringToList (){
+    int j = 0;
+     for (int i=0; i < textList.Length; i++){
+        if (textList[i].Length <= 3) {
+            textListLen3[j] = textList[i];
+            j++;
+        }
+    }
+}
+
+//Вызов функции
+AddStringToList();
+
+//Вывод на печать
+System.Console.WriteLine($"['{string.Join("', '", textListLen3)}']");
